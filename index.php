0716d5df699dec2f023d770e87b003cb3a2e508b
bixente <vhiribarren@gmail.com>
2018-07-05 18:08:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<head>
+ 
+<title>WASSCE Quiz</title>
+<link href="main.css"rel="stylesheet"type="text/css"/>
+<meta name=viewport content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+</head>
+<body>
+
+<div id="topbar">WASSCE Quiz</div>
+<div class="spacer"></div>
+
+<div id="homescreen">
+<h2>Welcome!</h2>
+<p class="description">This quiz is aimed at West African students taking West African Senior School Certificate Examination (WASSCE) History Paper 1, “West Africa and the Wider World from Earliest Times to 2000”. Don't forget to read <a href="https://wasscehistorytextbook.com/" target="_blank">our textbook</a> before answering the questions.</p>
+<button type="button" class="start" onclick="move()">Start Quiz</button>
+</div>
+
+<div id="navContent">
+<div id="game1"></div>
+<div id="game2"></div>
+</div>
+
+<script src="jquery.js"></script>
+<script src="controller.js"></script>
+<script>function move(){$('.start').parents('#homescreen').fadeOut();$('#navContent').fadeIn();}</script>
+<script type="text/javascript">
+    $('a').click(function(e) { 
+
+      if (this.href.substr(0,4) === 'https') {
+
+        e.preventDefault();
+
+        cordova.InAppBrowser.open(this.href, '_system');
+
+      }
+
+    });
+    </script>
+
+</body>
+</html>
